d47c33e31b8bd372eec92cdeee96cb61b641a996
Gabriella Massaro <gmassaro807@gmail.com>
2024-06-12 15:05:38 -0400

    Handle Value and Submit to console log state on AddToList

--- src/components/ItemForm.js
+++ src/components/ItemForm.js
@@ -1,12 +1,29 @@
-import React from "react";
+import React, { useState } from "react";
 import { v4 as uuid } from "uuid";
 
 function ItemForm(props) {
+  // const newItem = {
+  //   id: uuid(), // the `uuid` library can be used to generate a unique id
+  //   name: itemName,
+  //   category: itemCategory,
+  // };
+
+  const [newFood, setNewFood] = useState("");
+
+  function handleValue(event) {
+    setNewFood(event.target.value);
+  }
+
+  function handleSubmit(event) {
+    event.preventDefault()
+    console.log(newFood)
+  }
+
   return (
     <form className="NewItem">
       <label>
         Name:
-        <input type="text" name="name" />
+        <input type="text" name="name" onChange={handleValue}/>
       </label>
 
       <label>
@@ -17,8 +34,7 @@ function ItemForm(props) {
           <option value="Dessert">Dessert</option>
         </select>
       </label>
-
-      <button type="submit">Add to List</button>
+      <button type="submit" onClick={handleSubmit}>Add to List</button>
     </form>
   );
 }
