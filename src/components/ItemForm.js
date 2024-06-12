c46c6fe570c1e403ccdd55ec2bff486a979cf290
Gabriella Massaro <gmassaro807@gmail.com>
2024-06-12 17:45:19 -0400

    Reverted changes, console logg gives me my object

--- src/components/ItemForm.js
+++ src/components/ItemForm.js
@@ -1,26 +1,31 @@
 import React, { useState } from "react";
 import { v4 as uuid } from "uuid";
 
-function ItemForm(props) {
-  // const newItem = {
-  //   id: uuid(), // the `uuid` library can be used to generate a unique id
-  //   name: itemName,
-  //   category: itemCategory,
-  // };
+function ItemForm() {
 
-  const [newFood, setNewFood] = useState("");
+  const [food, setNewFood] = useState("");
+  const [category, setNewCategory] = useState("Produce");
 
   function handleValue(event) {
     setNewFood(event.target.value);
   }
 
+  function handleCategoryChange(event){
+    setNewCategory(event.target.value);
+  }
+
   function handleSubmit(event) {
     event.preventDefault()
-    console.log(newFood)
+    const newItem = ({
+      id: uuid(),
+      name: food,
+      category: category
+    })
+    console.log(newItem)
   }
 
   return (
-    <form className="NewItem">
+    <form className="NewItem" onSubmit={handleSubmit}>
       <label>
         Name:
         <input type="text" name="name" onChange={handleValue}/>
@@ -28,13 +33,13 @@ function ItemForm(props) {
 
       <label>
         Category:
-        <select name="category">
+        <select name="category" onChange={handleCategoryChange}>
           <option value="Produce">Produce</option>
           <option value="Dairy">Dairy</option>
           <option value="Dessert">Dessert</option>
         </select>
       </label>
-      <button type="submit" onClick={handleSubmit}>Add to List</button>
+      <button type="submit" >Add to List</button>
     </form>
   );
 }
