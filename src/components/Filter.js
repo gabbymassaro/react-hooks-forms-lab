8cd23d18ea35a0cb4ed68b414d20530ff47f1bae
Gabriella Massaro <gmassaro807@gmail.com>
2024-06-12 10:49:22 -0400

    Create state for search input

--- src/components/Filter.js
+++ src/components/Filter.js
@@ -1,9 +1,16 @@
-import React from "react";
+import React, { useState } from "react";
 
 function Filter({ onCategoryChange }) {
+
+  const [input, setInput] = useState("Search");
+
+  function handleInput(event) {
+    setInput(event.target.value)
+  }
+
   return (
     <div className="Filter">
-      <input type="text" name="search" placeholder="Search..." />
+      <input type="text" name="search" placeholder="Search..." value={input} onChange={handleInput}/>
       <select name="filter" onChange={onCategoryChange}>
         <option value="All">Filter by category</option>
         <option value="Produce">Produce</option>
