f731e18f88e002b40ddbf29ea6367401f5309c5a
Gabriella Massaro <gmassaro807@gmail.com>
2024-06-12 11:34:52 -0400

    Create a stored input that returns matching itemData

--- src/components/Search.js
+++ src/components/Search.js
@@ -3,14 +3,25 @@ import itemData from "../data/items";
 
 
 function Search() {
-
-  console.log(itemData)
-
+  let storedInput
   const [input, setInput] = useState("Search");
 
   function handleInput(event) {
     setInput(event.target.value)
   }
+
+  storedInput = []
+  itemData.filter(item => {
+    if (item.name === input) {
+      storedInput.push(item)
+    } else if (input === "Search") {
+      storedInput = [...itemData]
+    }
+    return storedInput
+  })
+
+  console.log(storedInput)
+
   return (
     <input type="text" name="search" placeholder="Search..." value={input} onChange={handleInput}/>
   )
